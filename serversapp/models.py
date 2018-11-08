74bf8b6e0c68914b5cee3afa4ceb5e7a546961dd
zarbul <dlyapicem@gmail.com>
2018-11-08 21:13:43 +0300

    add server and serverModel models

--- serversapp/models.py
+++ serversapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+class ServerModel(models.Model):
+    servermodelname = models.CharField(max_length=100)
+
+
+class Server(models.Model):
+    name = models.CharField(max_length=50)
+    netbiosname = models.CharField(max_length=100)
+    ip = models.GenericIPAddressField()
+    macadress = models.GenericIPAddressField()
+    manufacturer = models.CharField(max_length=50, choices={})
+    formfactor = models.CharField(max_length=50, choices={})
+    type = models.CharField(max_length=50)
+    servermodel_id = models.ForeignKey(ServerModel, on_delete=models.CASCADE)
+    # technic_id = models.ForeignKey(Technic)
