dad2ffd1bb00ae9f254e72fda10ce1689286bcc6
postman17 <frompostal@gmail.com>
2018-11-13 15:29:29 +0500

    add models Workstation and WorkstationModel

--- workstationsapp/models.py
+++ workstationsapp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Workstation(models.Model):
+
+    title = models.CharField(max_length=200)
+    model = models.ForeignKey(WorkstationModel, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Workstation'
+        verbose_name_plural = 'Workstations'
+
+    def __str__(self):
+        return self.title
+
+
+class WorkstationModel(models.Model):
+
+    model = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'WorkstationModel'
+        verbose_name_plural = 'WorkstationModels'
+
+    def __str__(self):
+        return self.model
